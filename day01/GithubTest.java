3f8d152a87555b87555d6b0969357195be8dd6c7
firebat <firebat@163.com>
2023-05-18 20:33:50 +0800

    xixi

--- day01/GithubTest.java
+++ day01/GithubTest.java
@@ -2,6 +2,6 @@ package day01;
 
 public class GithubTest {
     public static void main(String[] args) {
-        System.out.println("hellogit");
+        System.out.println("hellogit from firebat");
     }
 }
