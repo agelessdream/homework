05866f9bf1c5b00f554be0bff0e87cc974b39d89
revolution <revolution@163.com>
2023-05-18 20:02:17 +0800

    Initial commit

---
+++ day01/Homework3.java
@@ -0,0 +1,17 @@
+package day01;
+
+public class Homework3{
+    public static void main(String[] args){
+        String name="张三";
+        int age=22;
+        String gender="male";
+        double weight=60;
+        boolean beMarried=false;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(gender);
+        System.out.println(weight);
+        System.out.println(beMarried);
+
+    }
+}
