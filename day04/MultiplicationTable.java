c12c1e8ea61489fc434a1e558013b5a86d0c09d8
revolution <revolution@163.com>
2023-05-22 15:58:35 +0800

    202305221558

---
+++ day04/MultiplicationTable.java
@@ -0,0 +1,12 @@
+package day04;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        for(int i=1;i<10;i++){
+            for(int j=1;j<=i;j++){
+                System.out.printf("%d*%d=%-4d",j,i,i*j);
+            }
+            System.out.println();
+        }
+    }
+}
