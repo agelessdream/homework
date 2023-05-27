02d91ab9c5563d192240ffd61a478a1df3728c98
revolution <revolution@163.com>
2023-05-27 17:01:32 +0800

    202305271701

--- temp/BubbleSortCompare.java
+++ temp/BubbleSortCompare.java
@@ -7,6 +7,20 @@ public class BubbleSortCompare {
     static void bubbleSort(int[] arr) {
         int count = 0;
         for (int i = arr.length - 1; i > 0; i--) {
+            count++;
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("不优化后的趟数" + count);
+    }
+    static void bubbleSort2(int[] arr) {//优化
+        int count = 0;
+        for (int i = arr.length - 1; i > 0; i--) {
             boolean flag = true;
             count++;
             for (int j = 0; j < i; j++) {
@@ -24,35 +38,20 @@ public class BubbleSortCompare {
         System.out.println("优化后的趟数" + count);
     }
 
-    static void bubbleSort2(int[] arr) {
-        int count = 0;
-        for (int i = arr.length - 1; i > 0; i--) {
-            count++;
-            for (int j = 0; j < i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
-        System.out.println("不优化后的趟数" + count);
-    }
-
     public static void main(String[] args) {
-        int[] arr1 = new int[2000];
+        int[] arr1 = new int[100000];
 
         for (int i = 0; i < arr1.length; i++) {
-            arr1[i] = (int) (Math.random() * 50);
+            arr1[i] = (int) (Math.random() * 5555);
         }
 
         int[]arr2= Arrays.copyOf(arr1,arr1.length);
         long time1 = System.currentTimeMillis();
-        bubbleSort(arr1);//优化的冒泡排序
+        bubbleSort(arr1);
         long time2 = System.currentTimeMillis();
-        bubbleSort2(arr2);//不优化的冒泡
+        bubbleSort2(arr2);
         long time3 = System.currentTimeMillis();
-        System.out.println("优化后的冒泡排序的执行时间为" + (time2 - time1) + "毫秒");
-        System.out.println("不优化的冒泡排序的执行时间为" + (time3 - time2) + "毫秒");
+        System.out.println((time2 - time1) + "毫秒");
+        System.out.println( (time3 - time2) + "毫秒");
     }
 }
--- temp/ForTest.java
+++ temp/ForTest.java
@@ -2,11 +2,6 @@ package temp;
 
 public class ForTest {
     public static void main(String[] args) {
-        long[] arr=new long[100];
-        arr[0]=1;arr[1]=2;
-        for(int i=2;i<100;i++){
-            arr[i]=arr[i-1]+arr[i-2];
-        }
-        System.out.println(arr[99]);
+        System.out.println("\t ");
     }
 }
